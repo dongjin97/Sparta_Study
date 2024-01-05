e6bddcdb3bb2ef298361020f3bc64c7460b3b5c3
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-01-05 16:33:07 +0900

    위시리스트앱만들기 시작

---
+++ Ch3-3Task/WishList/WishList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishList
+//
+//  Created by 원동진 on 2024/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
