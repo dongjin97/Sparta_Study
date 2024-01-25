14aab3d4969ccd3082354d02c6d1923de9f0c5b5
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-01-25 18:14:17 +0900

    userIntroduce관련 UI 추가

--- individualTask/IndividualTask/ProfileDesignVC/ViewComponent/UserInfoView.swift
+++ individualTask/IndividualTask/ProfileDesignVC/ViewComponent/UserInfoView.swift
@@ -31,6 +31,24 @@ class UserInfoView : UIView {
     private lazy var postLabel = UILabel()
     private lazy var followerLabel = UILabel()
     private lazy var followingLabel = UILabel()
+    private lazy var userIntroduceLabel : UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.numberOfLines = 2
+        label.textColor = .black
+        label.setRangeTextFont(fullText: "르탄이\niOS Developer 🍎", range: "iOS Developer 🍎", uiFont: UIFont.systemFont(ofSize: 14))
+        return label
+    }()
+    private lazy var urlButton : UIButton =  {
+        var config = UIButton.Configuration.plain()
+        config.title = "spartacodingclub.kr"
+        config.baseForegroundColor = .systemBlue
+        config.contentInsets = NSDirectionalEdgeInsets.init(top: 0, leading: 0, bottom: 0, trailing: 0)
+        let button = UIButton(configuration: config)
+        button.contentHorizontalAlignment = .leading
+
+        return button
+    }()
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubViews()
@@ -67,17 +85,17 @@ extension UserInfoView{
         }
     }
     private func addSubViews(){
-        addSubViews([userImageView,userFollowInfoView])
+        addSubViews([userImageView,userFollowInfoView,userIntroduceLabel,urlButton])
         userFollowInfoView.addSubViews([labelCntStackView,postLabel,followerLabel,followingLabel])
         labelCntStackView.addStackSubViews([postCntLabel,followerCntLabel,followingCntLabel])
     }
     private func setAutoLayout(){
         userImageView.snp.makeConstraints { make in
-            make.top.left.bottom.equalToSuperview()
+            make.top.left.equalToSuperview()
             make.height.width.equalTo(88)
         }
         userFollowInfoView.snp.makeConstraints { make in
-            make.top.right.bottom.equalToSuperview()
+            make.top.right.equalToSuperview()
             make.left.equalTo(userImageView.snp.right).offset(41)
         }
         labelCntStackView.snp.makeConstraints { make in
@@ -101,6 +119,14 @@ extension UserInfoView{
             make.left.equalTo(followingCntLabel.snp.left)
             make.right.equalTo(followingCntLabel.snp.right)
             make.bottom.equalToSuperview()
+        }
+        userIntroduceLabel.snp.makeConstraints { make in
+            make.top.equalTo(userImageView.snp.bottom).offset(14)
+            make.left.right.equalToSuperview()
+        }
+        urlButton.snp.makeConstraints { make in
+            make.top.equalTo(userIntroduceLabel.snp.bottom)
+            make.left.right.bottom.equalToSuperview()
         }
     }
 }
